dba552449749e99a46795549a3fc5e6f91619275
Nils Wentzell <nwentzell@flatironinstitute.org>
2020-03-24 18:36:23 -0400

    Add test for h5 storable class, FAILING

---
+++ test/python/h5/storable.hpp
@@ -0,0 +1,28 @@
+#include <h5/h5.hpp>
+#include <string>
+#include <vector>
+
+class storable{
+  public:
+  // Some Data members
+  std::vector<int> vec = { 4, 2 };
+  std::string s = "😎";
+
+  // Default constructor has to be specified for cpp2py to pick it up
+  storable() = default;
+
+  // Our format string
+  static std::string hdf5_format(){ return "Storable"; }
+
+  friend void h5_write(h5::group g, std::string const & name, storable const & obj){
+    auto gr = g.create_group(name); 
+    h5_write(gr, "vec", obj.vec);
+    h5_write(gr, "s", obj.s);
+  }
+
+  friend storable h5_read(h5::group g, std::string const & name, storable & obj){
+    auto gr = g.open_group(name); 
+    h5_read(gr, "vec", obj.vec);
+    h5_read(gr, "s", obj.s);
+  }
+};
---
+++ test/python/h5/storable_desc.py
@@ -0,0 +1,50 @@
+# Generated automatically using the command :
+# c++2py storable.hpp -a storable -m storable -o storable --cxxflags="-std=c++17" --includes=./../../../c++ --includes=./../../../python --target_file_only
+from cpp2py.wrap_generator import *
+
+# The module
+module = module_(full_name = "storable", doc = r"", app_name = "storable")
+
+# Imports
+module.add_imports(*['_h5py'])
+
+# Add here all includes
+module.add_include("storable.hpp")
+
+# Add here anything to add in the C++ code at the start, e.g. namespace using
+module.add_preamble("""
+#include <cpp2py/converters/string.hpp>
+#include <cpp2py/converters/vector.hpp>
+
+""")
+
+
+# The class storable
+c = class_(
+        py_type = "Storable",  # name of the python class
+        c_type = "storable",   # name of the C++ class
+        doc = r"""""",   # doc of the C++ class
+        hdf5 = True,
+)
+
+c.add_member(c_name = "vec",
+             c_type = "std::vector<int>",
+             read_only= False,
+             doc = r"""""")
+
+c.add_member(c_name = "s",
+             c_type = "std::string",
+             read_only= False,
+             doc = r"""""")
+
+c.add_constructor("""()""", doc = r"""""")
+
+c.add_method("""std::string hdf5_format ()""",
+             is_static = True,
+             doc = r"""""")
+
+module.add_class(c)
+
+
+
+module.generate_code()
