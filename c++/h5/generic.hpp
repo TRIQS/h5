d0a4f6f74e6e2b93f3b1e3cbfc1ba8c700bb67ed
Nils Wentzell <nwentzell@flatironinstitute.org>
2020-03-31 19:19:56 -0400

    [h5] Add generic h5_read_attribute_from_key, Improve doc and formatting in h5/generic.hpp

--- c++/h5/generic.hpp
+++ c++/h5/generic.hpp
@@ -4,10 +4,10 @@
 namespace h5 {
 
   /**
-   * A generic read
-   * 
-   * @tparam T 
-   * @param g  HDF5 group
+   * A generic read of a key in a group
+   *
+   * @tparam T The C++ type to be read
+   * @param g The h5::group
    * @param key Name of the element
    * @return The value read from the file
    */
@@ -23,27 +23,44 @@ namespace h5 {
   }
 
   /**
-   * A generic attribute read.
-   * 
-   * @tparam T 
-   * @param obj The object to which the attribute is attached.
-   * @param key Name of the element
-   * @return The attribute name of obj, and "" if the attribute does not exist.
+   * A generic attribute read from an object
+   *
+   * @tparam T The C++ type of the attribute
+   * @param id The object id
+   * @param name Name of the attribute
+   * @return The attribute object, and "" if the attribute does not exist.
    */
   template <typename T>
-  T h5_read_attribute(h5_object obj, std::string const &key) {
+  T h5_read_attribute(hid_t id, std::string const &name) {
     T x;
-    h5_read_attribute(obj, key, x);
+    h5_read_attribute(id, name, x);
     return x;
   }
 
   /**
-   * Try a read
-   * 
-   * @tparam T 
+   * A generic attribute read from a key in an h5::group
+   *
+   * @tparam T The C++ type of the attribute
+   * @param g The h5::group
+   * @param key The key
+   * @param name Name of the attribute
+   * @return The attribute object, and "" if the attribute does not exist.
+   */
+  template <typename T>
+  T h5_read_attribute_from_key(group g, std::string const &key, std::string const &name) {
+    T x;
+    h5_read_attribute_from_key(g, key, name, x);
+    return x;
+  }
+
+  /**
+   * Try a generic read of a key in a group.
+   *
+   * @tparam T The C++ type to be read
    * @param g  HDF5 group
    * @param key Name of the element
    * @param x Parameter to read.
+   * @return true if the read succeeds, false if it fails
    */
   template <typename T>
   inline bool h5_try_read(group g, std::string key, T &x) {
