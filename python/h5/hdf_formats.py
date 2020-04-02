410cc73bed886c6d073ebffaa3058d8457612e46
Nils Wentzell <nwentzell@flatironinstitute.org>
2020-04-02 12:45:02 -0400

    Minor cleanings in hdf_formats.py

--- python/h5/hdf_formats.py
+++ python/h5/hdf_formats.py
@@ -20,41 +20,41 @@
 ################################################################################
 import re
 
-class HDF5Scheme:
-    def __init__(self, classname, modulename, doc, hdf_format, read_fun) : 
-        self.classname, self.modulename, self.doc, self.read_fun = classname, modulename,doc, read_fun
+class Format:
+    def __init__(self, classname, modulename, doc, hdf_format, read_fun) :
+        self.classname, self.modulename, self.doc, self.read_fun = classname, modulename, doc, read_fun
         self.format_name = hdf_format
 
-    def __str__(self) : 
+    def __str__(self) :
         return """
         Name of the class : %s
         Name of the module : %s
         Documentation : %s"""%(self.classname,self.modulename,self.doc)
-        
+
 _hdf5_formats_dict= {}
 
 def register_class (cls, doc = None, read_fun = None, hdf5_format = None):
     """
      For each class, register it with::
 
-         from HDFArchive_Schemes_dict import register_class 
+         from HDFArchive_Schemes_dict import register_class
          register_class (GfImFreq, doc= doc_if_different_from cls._hdf5_format_doc_ )
 
     """
-    hdf_format = hdf5_format or (cls._hdf5_format_ if hasattr(cls,"_hdf5_format_") else cls.__name__) 
+    hdf_format = hdf5_format or (cls._hdf5_format_ if hasattr(cls,"_hdf5_format_") else cls.__name__)
     assert hdf_format not in _hdf5_formats_dict, "class %s is already registered"%hdf_format
     doc = doc if doc else (cls._hdf5_format_doc_ if hasattr(cls,"_hdf5_format_doc_") else {})
-    _hdf5_formats_dict [hdf_format] = HDF5Scheme (cls.__name__, cls.__module__,doc, hdf_format, read_fun)
+    _hdf5_formats_dict[hdf_format] = Format(cls.__name__, cls.__module__,doc, hdf_format, read_fun)
 
 
 # Backward compatibility layer
 _hdf5_formats_bck_compat = [] # List of (regex, format, lambda)
 
-def register_backward_compatibility_method(regex, clsname, fun):  
+def register_backward_compatibility_method(regex, clsname, fun):
     """
     regex : the regular expression to match the hdf_format (e.g. "Gf" for GfImfreq_x_whatever....)
     clsname : the class name that it corresponds to
-    fun : a lambda taking hdf_format and returning a dict 
+    fun : a lambda taking hdf_format and returning a dict
           field_name -> hdf_format
           to read old data where not every subobjects have a hdf_format.
     """
@@ -62,29 +62,28 @@ def register_backward_compatibility_method(regex, clsname, fun):
 
 # Functions used in the hdf_archive implementation
 
-def hdf_format_access_for_write(hdf_format): 
-    """Given a hdf_format, return the format. Must match exactly. Used in WRITING"""
-    # If present exactly, we return it
-    if hdf_format in _hdf5_formats_dict : 
-        return _hdf5_formats_dict[hdf_format]
-    raise KeyError("HDF5 Data Scheme %s is not registered"%hdf_format)
-
-def hdf_format_access_for_read (hdf_format): 
+def hdf_format_access_for_write(hdf_format):
+    """Given an hdf_format, return the associated Format object.
     """
-    Given a hdf_format, return the format, result of compat func or None. 
+    # If present exactly, we return it
+    if hdf_format in _hdf5_formats_dict:
+        return _hdf5_formats_dict[hdf_format]
+    raise KeyError("HDF5 Format %s is not registered"%hdf_format)
+
+def hdf_format_access_for_read(hdf_format):
+    """
+    Given a hdf_format, return the format, result of compat func or None.
     Using compatibility layer. Used in READING
     """
     # If present exactly, we return it
-    if hdf_format in _hdf5_formats_dict : 
+    if hdf_format in _hdf5_formats_dict :
         return _hdf5_formats_dict[hdf_format], None
-    
+
     # Enter compatibility mode.
     l = [(r,s,l) for (r,s,l) in _hdf5_formats_bck_compat if re.match(r, hdf_format)]
-    if len(l) ==0 : 
-        raise KeyError("HDF5 Data Scheme %s is not registered and no backward compatibility found"%hdf_format)
-    if len(l) >1 : 
-        raise KeyError("HDF5 Data Scheme %s : ambiguous backward compatibility layers : %s"%([r for (r,s,ll) in l]))
+    if len(l) ==0 :
+        raise KeyError("H5 Format %s is not registered and no backward compatibility found"%hdf_format)
+    if len(l) >1 :
+        raise KeyError("H5 Format %s : ambiguous backward compatibility layers : %s"%([r for (r,s,ll) in l]))
     r,s,l = l[0]
     return s, l(hdf_format)
-
-
