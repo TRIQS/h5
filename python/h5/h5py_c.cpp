0a51fc3d3f1842fbd7bcd85e6430971681d5fe37
Olivier Parcollet <olivier.parcollet@cea.fr>
2020-09-18 11:50:53 -0400

    Fix error in new code

--- python/h5/h5py_c.cpp
+++ python/h5/h5py_c.cpp
@@ -13,7 +13,6 @@ using namespace std::string_literals;
 #define CHECK_OR_THROW(Cond, Mess)                                                                                                                   \
   if (!(Cond)) throw std::runtime_error("Error in h5 interface : "s + Mess);
 
-
 #define AS_STRING(...) AS_STRING2(__VA_ARGS__)
 #define AS_STRING2(...) #__VA_ARGS__
 
@@ -33,20 +32,20 @@ namespace h5 {
   // -------------------------
 
   PyObject *memory_file::as_py_byte_array() const {
-    size_t image_len = get_size();
-    PyObject *py_byte_array  = PyByteArray_FromStringAndSize(" ", Py_ssize_t(image_len));
-    //auto err         = PyByteArray_Resize(pybuf, );
-    //CHECK_OR_THROW(err, "Can not resize the PyByteArray ?!");
+    size_t image_len        = get_size();
+    PyObject *py_byte_array = PyByteArray_FromStringAndSize(" ", Py_ssize_t(1));
+    //PyObject *py_byte_array  = PyByteArray_FromStringAndSize(" ", Py_ssize_t(image_len));
+    int err = PyByteArray_Resize(py_byte_array, Py_ssize_t(image_len));
+    CHECK_OR_THROW(not err, "Can not resize the PyByteArray ?!");
+    CHECK_OR_THROW((PyByteArray_Size(py_byte_array) == image_len), "Incorrect size of PyByteArray");
 
-    hssize_t free_space = H5Fget_freespace(this->id);
-
-    PRINT(free_space);
-    PRINT(PyByteArray_Size(py_byte_array)); 
-    CHECK_OR_THROW( (PyByteArray_Size(py_byte_array) == image_len), "Incorrect size of PyByteArray");
+    //hssize_t free_space = H5Fget_freespace(this->id);
+    //PRINT(free_space);
+    //PRINT(PyByteArray_Size(py_byte_array));
     write_to_buffer((unsigned char *)PyByteArray_AsString(py_byte_array), image_len);
     return py_byte_array;
   }
 
   // -----------------------------
 
-}
+} // namespace h5
