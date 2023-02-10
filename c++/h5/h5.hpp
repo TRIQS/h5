ea24e122045f75d06f24858210ccd0cacae9569f
Olivier Parcollet <olivier.parcollet@cea.fr>
2023-02-10 17:57:07 -0500

    Add concept H5Storable

--- c++/h5/h5.hpp
+++ c++/h5/h5.hpp
@@ -18,6 +18,7 @@
 #define LIBH5_H5_HPP
 
 #include "./std_addons/complex.hpp"
+#include <concepts>
 
 #include "./file.hpp"
 #include "./group.hpp"
@@ -44,5 +45,16 @@
    || (H5_VERS_MAJOR > Maj))
 
 #endif
+
+namespace h5 {
+
+  template <typename T>
+  concept H5Storable = requires(T const &xc, T &x, h5::group g, std::string const &subgroup_name) {
+                         { get_hdf5_format(xc) } -> std::convertible_to<std::string>;
+                         { write(g, subgroup_name, xc) };
+                         { read(g, subgroup_name, x) };
+                       };
+
+} // namespace h5
 
 #endif // LIBH5_H5_HPP
