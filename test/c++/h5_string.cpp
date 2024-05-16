6ba46e028eb07faec52c46b372cfcebcd25312c3
gen_copyright <mail@server.com>
2024-05-16 10:52:29 -0400

    Regenerate Apache copyright headers

--- test/c++/h5_string.cpp
+++ test/c++/h5_string.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Simons Foundation
+// Copyright (c) 2020-2024 Simons Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// Authors: Miles Stoudenmire, Nils Wentzell
+// Authors: Thomas Hahn, Miles Stoudenmire, Nils Wentzell
 
 #include <gtest/gtest.h>
 #include <h5/h5.hpp>
