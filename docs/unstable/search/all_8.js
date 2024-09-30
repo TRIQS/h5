e5eda869893db14888ea0d07b13cd3ea89d2b577
Thoemi09 <thomas.hahn01@gmail.com>
2024-09-30 22:50:28 +0000

    Deploying to github.io from @ TRIQS/h5@9466605c0546f0561a8051bad32add630b348d09 🚀

--- docs/unstable/search/all_8.js
+++ docs/unstable/search/all_8.js
@@ -5,6 +5,6 @@ var searchData=
   ['example_201_3a_20write_20read_20a_202_20dimensional_20array_2',['Example 1: Write/Read a 2-dimensional array',['../ex1.html',1,'']]],
   ['example_202_3a_20write_20read_20a_20custom_20c_20type_3',['Example 2: Write/Read a custom C++ type',['../ex2.html',1,'']]],
   ['example_203_3a_20serialization_4',['Example 3: Serialization',['../ex3.html',1,'']]],
-  ['examples_5',['Compiling the examples',['../examples.html#compiling',1,'']]],
-  ['examples_6',['Examples',['../examples.html',1,'']]]
+  ['examples_5',['Examples',['../examples.html',1,'']]],
+  ['examples_6',['Compiling the examples',['../examples.html#compiling',1,'']]]
 ];
