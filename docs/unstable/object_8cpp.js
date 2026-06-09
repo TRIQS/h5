86455d5db43ac7cb0c8ce2dc400ead9b12b2728e
Wentzell <Wentzell@users.noreply.github.com>
2026-06-09 17:35:15 +0000

    Deploying to github.io from @ TRIQS/h5@9f77be53a70095dbcb948f299c98f71ad5f77927 🚀

--- docs/unstable/object_8cpp.js
+++ docs/unstable/object_8cpp.js
@@ -1,5 +1,6 @@
 var object_8cpp =
 [
+    [ "h5::get_hdf5_attribute_type", "group__h5__types.html#ga3e22dda1735a1c28aeef30284219fc7d", null ],
     [ "h5::get_hdf5_type", "group__h5__types.html#ga9663a6cefb9589bafc7185f7c45563fc", null ],
     [ "h5::get_name_of_h5_type", "group__h5__types.html#ga9b4181108d25ba467cf17dd150d9fb9a", null ],
     [ "h5::hdf5_type_equal", "group__h5__types.html#ga1f1926665da46101206e6ffdddb19600", null ]
--- docs/unstable/object_8hpp.js
+++ docs/unstable/object_8hpp.js
@@ -5,7 +5,9 @@ var object_8hpp =
     [ "h5::dataspace", "group__data__model.html#ga7702bf25897ae4f687e0f6e2ffd49a4d", null ],
     [ "h5::datatype", "group__data__model.html#ga177e858254a3d1ab84a707a225fa4b1f", null ],
     [ "h5::proplist", "group__data__model.html#gaaeba12898bf632f53beb56f017165142", null ],
+    [ "h5::get_hdf5_attribute_type", "group__h5__types.html#ga3e22dda1735a1c28aeef30284219fc7d", null ],
     [ "h5::get_hdf5_type", "group__h5__types.html#ga9663a6cefb9589bafc7185f7c45563fc", null ],
+    [ "h5::get_name_of_h5_type", "group__h5__types.html#ga9ff570e8fc0a7937424ac3d8a075129c", null ],
     [ "h5::get_name_of_h5_type", "group__h5__types.html#ga9b4181108d25ba467cf17dd150d9fb9a", null ],
     [ "h5::hdf5_type", "group__h5__types.html#ga92724d6ad97c1cbab14a73477693620d", null ],
     [ "h5::hdf5_type_equal", "group__h5__types.html#ga1f1926665da46101206e6ffdddb19600", null ]
